faec14906edecae33bfa1d077b6aa3209dc4b0ce
Vitaly <143294986+Vitaly1962@users.noreply.github.com>
2023-11-21 02:18:04 +0500

    Add files via upload

---
+++ DZ/DZ_9.1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: 
+// Задайте значения M и N. Напишите программу,которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int ReadInt(string mes)
+{
+    Console.WriteLine(mes);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintNaturalNum(int M, int N)
+{
+    if (N < M )
+    {
+        return ;
+    }
+    if(M % 2 == 0)
+    {
+    System.Console.Write($"{M}\t");
+    }
+    PrintNaturalNum(M + 1, N);
+}
+
+int M = ReadInt("Введите первое число");
+int N = ReadInt("Введите второе число");
+PrintNaturalNum(M, N);
